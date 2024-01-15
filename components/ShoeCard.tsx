b17475d56b64abd3a4ef0a598eba7e1892c78bd8
Kanisevsky <meltsmokestudio@gmail.com>
2024-01-15 20:05:34 +0000

    Hero

--- components/ShoeCard.tsx
+++ components/ShoeCard.tsx
@@ -1,14 +1,26 @@
+import Image from "next/image";
+
 interface ShoeCardProps {
   imgUrl: string;
-  changeBigShoeImage: () => void;
+  changeBigShoeImage: ({}) => void;
   bigShoeImg: string
 }
 
 
 function ShoeCard({imgUrl, changeBigShoeImage, bigShoeImg} : ShoeCardProps ) {
+  const handleClick = () => {
+    if(bigShoeImg !== imgUrl) {
+      changeBigShoeImage(imgUrl)
+    }
+  }
   return (
-    <div>
-      
+    <div className={`border-2 rounded-xl cursor-pointer max-sm:flex-1
+    ${bigShoeImg === imgUrl ? "border-coral-red" : "border-transparent" }`}
+    onClick={handleClick}
+    >
+      <div className="">
+        <Image src={"/"} alt="" width={127} height={103}/>
+      </div>
     </div>
   )
 }
